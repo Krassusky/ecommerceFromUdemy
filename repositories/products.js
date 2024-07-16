8405de636c351771d0368aa68e6fac8216f74119
Krassusky <barroskrassusky@gmail.com>
2024-07-16 15:38:36 -0300

    still fixing the image input

--- repositories/products.js
+++ repositories/products.js
@@ -2,27 +2,9 @@ const Repository = require ('./repository');
 const fs = require('fs');
 
 class ProductsRepository extends Repository{
-    // constructor() {
-    //     if(!filename){
-    //         throw new Error('Creating a repository requires a filename "products"')
-    //     }
-    //     this.filename = filename;
-    //     try{
-    //         fs.accessSync(this.filename);
-    //     } catch (err){
-    //         fs.writeFileSync(this.filename, '[]');
-    //     }
+    
 
-    // }
-    // async create(attrs){
-    //     attrs.id = this.randomId();
-
-    //     const products = await this.getAll();
-
-    //     products.push(attrs);
-
-    // }
-
+    
 
 }
 
